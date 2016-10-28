e97bd1734e8589605e388d7302bf46abd723e264
dkRaptor <dhrtklpsh82@gmail.com>
2016-10-29 02:08:32 +0530

    First Commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(document).ready(function() {
+ 
+  $("#tab-slideshow").owlCarousel({
+    navigation : false,
+    singleItem : true,
+	autoPlay: true,
+	pagination:false
+  });
+ 
+});
